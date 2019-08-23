f1f4c0aa8af3c677f6ded630bbb0f30aad4a920f
juvelist <39278128+juvelist@users.noreply.github.com>
2019-08-23 22:17:27 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // typewriter
 
 $(".header__main_text-question").typewriter({
-    text: "Yup, we can du that.",
+    text: "Yup, we can do that.",
     waitingTime: 1000,
     delay: 100,
     cursor: true,
